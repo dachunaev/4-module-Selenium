0706d9d2787aa2b1927495543e123efb067968ea
dachunaev <dmitrii@mitht.ru>
2020-01-14 17:28:39 +0300

    изменен метод по проверке текущего url страницы авторизации

--- pages/login_page.py
+++ pages/login_page.py
@@ -9,7 +9,7 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        assert "login" in f"{self.url}", "wrong url, should be login page"
+        assert "login" in f"{self.browser.current_url}", "wrong url, should be login page"
 
     def should_be_login_form(self):
         assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form not found"
