04066d0ba90e4cf6a65d557af5529ebcb4346f45
dachunaev <dmitrii@mitht.ru>
2020-01-16 18:14:48 +0300

    добавлен метод для перехода в корзину

--- pages/base_page.py
+++ pages/base_page.py
@@ -20,6 +20,10 @@ class BasePage():
         link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
         link.click()
 
+    def go_to_basket_page(self):
+        link = self.browser.find_element(*BasePageLocators.BASKET_LINK)
+        link.click()
+
     def get_text(self, how, what):
         assert self.is_element_present(how, what), f"{what} not found"
         text = self.browser.find_element(how, what).text
