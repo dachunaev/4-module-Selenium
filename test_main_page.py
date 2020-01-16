b72416636f4b944d271ffbe5aa9db07f56537147
dachunaev <dmitrii@mitht.ru>
2020-01-16 18:09:46 +0300

    тест для проверки перехода в корзину для гостя

--- test_main_page.py
+++ test_main_page.py
@@ -2,6 +2,7 @@ from pages.main_page import MainPage
 from pages.locators import MainPageLocators
 from .pages.login_page import LoginPage
 
+link = "http://selenium1py.pythonanywhere.com"
 
 def go_to_login_page(browser):
     login_link = browser.find_element_by_css_selector(*MainPageLocators.LOGIN_LINK)
@@ -9,7 +10,6 @@ def go_to_login_page(browser):
 
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com"
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
@@ -18,7 +18,12 @@ def test_guest_can_go_to_login_page(browser):
 
 
 def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_basket_page()
