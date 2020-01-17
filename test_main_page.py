6f946a981d2354927e59d7ae0c31342184532367
dachunaev <dmitrii@mitht.ru>
2020-01-17 13:55:58 +0300

    добвалены проверки для теста перехода гостя с главной страницы в корзину

--- test_main_page.py
+++ test_main_page.py
@@ -1,13 +1,8 @@
 from pages.main_page import MainPage
-from pages.locators import MainPageLocators
-from .pages.login_page import LoginPage
+from pages.login_page import LoginPage
+from pages.basket_page import BasketPage
 
 link = "http://selenium1py.pythonanywhere.com"
-
-
-def go_to_login_page(browser):
-    login_link = browser.find_element_by_css_selector(*MainPageLocators.LOGIN_LINK)
-    login_link.click()
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -28,3 +23,6 @@ def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     page = MainPage(browser, link)
     page.open()
     page.go_to_basket_page()
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.should_be_no_items_in_basket()
+    basket_page.should_be_empty_basket_text_message()
