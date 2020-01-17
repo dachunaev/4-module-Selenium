18e5dbcdeb418c9655388e5dba1aeec2de87c10b
dachunaev <dmitrii@mitht.ru>
2020-01-17 17:34:19 +0300

    добавлен класс с тестами для авторизованых пользователей

--- test_product_page.py
+++ test_product_page.py
@@ -1,10 +1,37 @@
 from pages.product_page import ProductPage
 from pages.basket_page import BasketPage
+from pages.login_page import LoginPage
 import pytest
+import faker
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+link_reg = "http://selenium1py.pythonanywhere.com/accounts/login/"
 # pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?"
 #                                                "promo=offer7", marks=pytest.mark.xfail),
+
+
+@pytest.mark.login_user
+class TestUserAddToBasketFromProductPage():
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        f = faker.Faker()
+        email = f.email()
+        password = "R17305084"
+        page = LoginPage(browser, link_reg)
+        page.open()
+        page.register_new_user(email, password)
+        page.should_be_authorized_user()
+
+    def test_user_cant_see_success_message(self, browser):
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_not_be_success_message()
+
+    def test_user_can_add_product_to_basket(self, browser):
+        page = ProductPage(browser, link)
+        page.open()
+        page.add_product_to_basket()
+        page.should_be_added()
 
 
 def test_guest_can_add_product_to_basket(browser):
