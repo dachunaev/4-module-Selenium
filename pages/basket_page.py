58a7dd4e3df647c26b0b6225097af1f52080f3cc
dachunaev <dmitrii@mitht.ru>
2020-01-17 13:53:18 +0300

    Добавлены: проверки наличая сообщения о пустой козине и его корректность для английской версии; проверка товаров в корзине на отсутствие

--- pages/basket_page.py
+++ pages/basket_page.py
@@ -1,7 +1,17 @@
 from .base_page import BasePage
-from .locators import BasePageLocators
+from .locators import BasketPageLocators
 
 
 class BasketPage(BasePage):
-    def should_be_empty_basket(self):
-        pass
+    def should_be_no_items_in_basket(self):
+        assert self.is_not_element_present(*BasketPageLocators.BASKET_ITEMS)
+
+    def should_be_empty_basket_text_message(self):
+        assert self.is_element_present(*BasketPageLocators.BASKET_IS_EMPTY_MESSAGE)
+        if "/en-gb/" in self.browser.current_url:
+            self.should_be_empty_basket_text_message_correct()
+
+    def should_be_empty_basket_text_message_correct(self):
+        should_be_text_message = "Your basket is empty"
+        current_text_message = self.get_text(*BasketPageLocators.BASKET_IS_EMPTY_MESSAGE)
+        assert should_be_text_message in current_text_message, "text message about empty basket incorrect"
