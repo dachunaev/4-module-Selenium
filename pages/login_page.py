c7aa45b7b2bec00eca8bdb034def464c44c788cf
dachunaev <dmitrii@mitht.ru>
2020-01-17 17:32:54 +0300

    добавлен метод для регистрации пользователя

--- pages/login_page.py
+++ pages/login_page.py
@@ -3,6 +3,16 @@ from .locators import LoginPageLocators
 
 
 class LoginPage(BasePage):
+    def register_new_user(self, email, password):
+        email_input = self.browser.find_element(*LoginPageLocators.REG_INPUT_EMAIL)
+        password_input = self.browser.find_element(*LoginPageLocators.REG_INPUT_PASSWORD)
+        confirm_password_input = self.browser.find_element(*LoginPageLocators.REG_INPUT_CONFIRM_PASSWORD)
+        email_input.send_keys(email)
+        password_input.send_keys(password)
+        confirm_password_input.send_keys(password)
+        button_reg = self.browser.find_element(*LoginPageLocators.REG_BUTTON)
+        button_reg.click()
+
     def should_be_login_page(self):
         self.should_be_login_url()
         self.should_be_login_form()
